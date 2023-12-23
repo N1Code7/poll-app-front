87897cde5b423e34708ba195002d147a6cbf5c31
N1Code7 <n1code7@outlook.fr>
2023-12-23 16:32:02 +0100

    🚚 add initial pages

---
+++ src/app/(home)/UnidentifiedPoll.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Squircle } from "corner-smoothing";
+
+const UnidentifiedPoll = () => {
+  return (
+    <Squircle as={"div"} cornerRadius={8}>
+      <form></form>
+    </Squircle>
+  );
+};
+
+export default UnidentifiedPoll;
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/layouts/Header";
+import UnidentifiedPoll from "./UnidentifiedPoll";
+
+const Home = async () => {
+  return (
+    <div>
+      <Header />
+      <UnidentifiedPoll />
+    </div>
+  );
+};
+
+export default Home;
